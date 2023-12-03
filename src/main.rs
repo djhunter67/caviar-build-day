4b357f50e26a39b8787b1590ca84d6815331f5df
Christerpher Hunter <djhunter67@gmail.com>
2023-12-03 07:26:03 -0500

    add: static folder

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,35 @@
+use actix_files::NamedFile;
+use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use log::debug;
+use simplelog::{CombinedLogger, Config, File, LevelFilter, WriteLogger};
+
+async fn send_css_file() -> HttpResponse {
+    let css_file_data = include_str!("static/css/style.css");
+    HttpResponse::Ok()
+        .content_type("text/css")
+        .body(css_file_data)
+}
+
+async fn send_fav_icon() -> actix_web::Result<actix_files::NamedFile> {
+    Ok(NamedFile::open("static/assets/images/favicon.png")?)
+}
 
 #[actix_web::main]
 async fn main() {
+    match CombinedLogger::init(vec![WriteLogger::new(
+        log::LevelFilter::Debug,
+        simplelog::Config::default(),
+        File::create("caviar.log").unwrap(),
+    )]) {
+        Ok(_) => debug!("Logger initialized"),
+        Err(e) => debug!("Logger failed to initialize: {}", e),
+    }
+
     let _ = HttpServer::new(|| {
         App::new()
+            .wrap(Logger::default())
+            .service(fs::Files::new("/static", ".").show_files_listing())
             .route("/", web::get().to(index))
             .route("/hello", web::get().to(hello))
     })
