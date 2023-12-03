997ba2ec094c13db8fef6cf1c9a0da7747ff8247
Christerpher Hunter <djhunter67@gmail.com>
2023-12-03 07:04:11 -0500

    add: basic html structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+#[actix_web::main]
+async fn main() {
+    let _ = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+            .route("/hello", web::get().to(hello))
+    })
+    .bind("0.0.0.0:8080")
+    .expect("Can not bind to port 8080")
+    .run()
+    .await;
+}
+
+struct Login {
+    username: String,
+    password: String,
+}
+
+struct CreateUser {
+    username: String,
+    password: String,
+    email: String,
+}
+
+struct UpdateUser {
+    username: String,
+    password: String,
+    email: String,
+}
+
+struct DeleteUser {
+    username: String,
+}
+
+async fn login() -> impl Responder {
+    HttpResponse::Ok().body("Login")
+}
+
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
 }
